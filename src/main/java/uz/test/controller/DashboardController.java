9ab8fe2f191afef064b9b1b2e923e8e54f6fe913
Akbar <otaboevakbar96@gmail.com>
2019-12-24 22:56:12 +0300

    dashboard controller

--- src/main/java/uz/test/controller/DashboardController.java
+++ src/main/java/uz/test/controller/DashboardController.java
@@ -2,7 +2,13 @@ package uz.test.controller;
 
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,5 +25,21 @@ public class DashboardController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        company.setOnMouseClicked(mouseEvent -> {
+            loadStage("/fxml/main.fxml");
+        });
+
+    }
+    private void loadStage(String fxml){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource(fxml));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
